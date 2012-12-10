1f994a95c86c523bc8372aff62674e6e994a6308
justin-esposito <jespo29@aol.com>
2012-12-09 21:18:35 -0500

    final project..

--- locations.js
+++ locations.js
@@ -1,17 +1,36 @@
-			//****removing NORTH, SOUTH, EAST,&WEST, going to use curentLocation for everything****
-			//replacing old system fully, removed score in this build fully since it was not kept to date
-			var currentLocation = "default";
-			var NORTH = 1;
-			var SOUTH = 11;
-			var EAST = 21;
-			var WEST = 31;
-			var direction = ""
+			var currentLocation = "defaultloc"
+			var NORTH = 0
+			var SOUTH = 1
+			var EAST = 2
+			var WEST = 3
 			var invDisplay = ""
 			var inventory = new Array;
+			var locationArray = new Array;
+			var frag1Check = 0
+			var frag2Check = 0
+			var frag3Check = 0
+			var frameCheck = 0
+			var moveMatrix = new Array(
+		
 			
-			
+												//direction 0,1,2,3
+	                     /* 0default */     ["north1", "south1", "east1", "west1"],
+	                     /* 1north1 */     [ "north2", "defaultloc", "falseloc", "falseloc"],
+	                     /* 2north2 */     ["falseloc", "north1", "falseloc", "falseloc"],
+	                     /* 3south1 */     ["defaultloc", "south2", "falseloc", "falseloc"],
+ /*currentLocation */    /* 4south2 */     [ "south1", "falseloc", "falseloc", "falseloc"],
+	                     /* 5east1 */     ["falseloc", "falseloc", "east2", "defaultloc"],
+	                     /* 6east2 */     ["falseloc", "falseloc", "falseloc", "east1"],
+	                     /* 7west1*/     ["falseloc", "falseloc", "defaultloc", "west2"],
+	                     /* 8west2*/     [ "northwest1", "falseloc", "west1", "falseloc"],
+	                     /* 9northwest1*/     [ "falseloc", "west2", "winroom", "northwest2"]
+						 /*10winroom*/ ["falseloc", "falseloc", "falseloc", "falseloc"]
+						 
+	  
+		
+			)
 			function init() {
-				updateText("You wake up in a room lying down on a couch. You have no recollection of what happened. Getting up off of the couch, you walk to the middle of the room.");
+				updateText("These rooms are colorful. Somehow you know to find three key fragments and a key frame.");
 			}
 			
 			function txtcmds() {
@@ -25,13 +44,13 @@
 				   move(EAST);
 				} else if ((check.value === "w") || (check.value === "W")) {
 				   move(WEST);
-				} else if ((check.value === "take item x") && (currentLocation === "default")){
+				} else if ((check.value === "take fragment 1") && (currentLocation === "north2")){
 					take(check.value);
-				} else if ((check.value === "take item y") && (currentLocation === "east1")){
+				} else if ((check.value === "take fragment 2") && (currentLocation === "east1")){
 					take(check.value);
-				} else if ((check.value === "take item z") && (currentLocation === "west1")){
+				} else if ((check.value === "take fragment 3") && (currentLocation === "west1")){
 					take(check.value);
-				} else if ((check.value === "take item a") && (currentLocation === "northwest1")){
+				} else if ((check.value === "take key frame") && (currentLocation === "south2")){
 					take(check.value);
 				} else if ((check.value === "help") || (check.value === "Help") || (check.value === "HELP")) {
 					updateText("insert cmdlist");
@@ -42,247 +61,50 @@
 				}
 			}
 	
+			function newLocations() {
 			
-			/*//generic move function
-			function move(direction) {
-				if	(direction === NORTH) {
-					if (currentLocation === "south1") {
-					currentLocation = "default";
-				} else if (currentLocation === "default") {
-					currentLocation = "north1"
-				} else if (currentLocation === "north1") {
-					currentLocation = "north2"
-			}
-			}else if (direction === SOUTH) {
-					if (currentLocation === "north1") {
-					currentLocation = "default";
-				} else if (currentLocation === "default"){
-					currentLocation = "south1";
-				} else if (currentLocation === "north2") {
-					currentLocation === "north1";
-			}
-			} else if (direction === EAST) {
-					if (currentLocation === "west1") {
-					currentLocation = "default";
-				} else if (currentLocation === "default") {
-					currentLocation = "east1";
-			}
-			} else if (direction === WEST) {
-					if (currentLocation === "east1") {
-					currentLocation === "default";
-					} else if (currentLocation === "default") {
-					currentLocation = "west1"
-			} 
-			}else {
-					updateText("Unknown direction" + direction)
+  		location0 = new Location("defaultloc", "Black room", "This room is black.", "No item.");
+  		location1 = new Location("north1", "Blue Room", "This room is blue. ", "No item.");
+  		location2 = new Location("north2", "Teal Room", "This room is teal.", "fragment 1");
+  		location3 = new Location("south1", "Red Room", "This room is red.", "No item.");
+  		location4 = new Location("south2", "Crimson Room", "This room is crimson.", "key frame");
+  		location5 = new Location("east1", "Green Room", "This room is green.", "fragment 2");
+  		location6 = new Location("east2", "Lime Room", "This room is lime.", "No item.");
+  		location7 = new Location("west1", "Yellow Room", "This room is yellow.", "fragment 3");
+  		location8 = new Location("west2", "Orange Room", "This room is orange.", "No item.");
+  		location9 = new Location("northwest1", "Rainbow Room", "This room is rainbow. What could it mean? Maybe it has to do something with a locked door to the west.", "No item.");
+  		location10 = new Location("winroom", "Coolbeans", "man you put that key together and was all like i win", "Stick of swag that you cant pick up because you won, how unfortunate");
+
+  		locationArray["defaultloc"] = location0;
+  		locationArray["north1"] = location1;
+  		locationArray["north2"] = location2;
+  		locationArray["south1"] = location3;
+  		locationArray["south2"] = location4;
+  		locationArray["east1"] = location5;
+  		locationArray["east2"] = location6;
+  		locationArray["west1"] = location7;
+  		locationArray["west2"] = location8;
+  		locationArray["northwest1"] = location9;
+		locationArray["winroom"] = location10
+		}
+		//i've literally tried troubleshooting this for several hours a day.
+		function move(direction) {
+			direction = direction;
+			var newLocation = moveMatrix[currentLocation][direction];
+			
+			if (newLocation === "winroom" && frag1Check = 0 || frag2Check = 0 ||frag3Check = 0 || frameCheck =0) {
+				currentLocation = "northwest1"
+				updateText("You're missing something.")
+				}else if (newLocation !== "falseloc" || newLocation !== "winroom") {
+					currentLocation = newLocation
+					newLocations();
+					updateText(locationArray[currentLocation]);
+				} else { 
+					updateText("There's a wall.")
 				}
-				updateLocation();
-				}*/
-				
-				function move(direction) {
-          switch (direction) {
-            case NORTH:
-              switch(currentLocation) {
-				case "west2":
-				   currentLocation = "northwest1"
-				   northwest1();
-				break;
-				
-                case "south2":
-                  currentLocation = "south1"
-                  south1();
-                break;
-                  
-                case "south1":
-                  currentLocation = "default";
-                  defaultloc();
-                break;
-                  
-                case "default":
-                  currentLocation = "north1";
-                  north1();
-                break;
-                 
-                case "north1":
-                  currentLocation = "north2"
-                  north2();
-                break;
-                    
-                 default:
-                   updateText ("Unknown direction" + direction)
-                 }
-            break;
-                        
-            case SOUTH:
-                  switch(currentLocation){
-					case "northwest1":
-					   currentLocation = "west2"
-					   west2();
-					break;
-					
-                    case "north2":
-                      currentLocation = "north1"
-                      north1();
-                    break;
-                    
-                    case "north1":
-                      currentLocation = "default"
-                      defaultloc();
-                    break;
-                    
-                    case "default":
-                      currentLocation = "south1"
-                      south1();
-                    break; 
-                    
-                    case "south1":
-                      currentLocation = "south2"
-                      south2();
-                    break;
-                    
-                    default:
-                      updateText ("Unknown direction" + direction)
-                    }
-                    break;
-                  
-                case EAST:
-                  switch(currentLocation){
-                    case "west2":
-                      currentLocation = "west1"
-                      west1();
-                    break;
-                   
-                    case "west1":
-                      currentLocation = "default"
-                      defaultloc();
-                    break;
-                    
-                    case "default":
-                      currentLocation = "east1"
-                      east1();
-                    break;
-                    
-                    case "east1":
-                      currentLocation = "east2"
-                      east2();
-                    break;
-                    
-                    default:
-                      updateText ("Unknown direction" + direction)
-                    }
-                    break;
-                    
-                  case WEST:
-                    switch(currentLocation){
-                      case "east2":
-                        currentLocation = "east1"
-                        east1();
-                      break;
-                      
-                      case "east1":
-                         currentLocation = "default"
-                         defaultloc();
-                       break;
-                         
-                       case "default":
-                          currentLocation = "west1"
-                          west1();
-                       break;
-                       
-                       case "west1":
-                          currentLocation = "west2"
-                          west2();
-                       break;
-                        
-                       default:
-                      updateText ("Unknown direction" + direction)
-                    }
-                    break;
-                   }
-				   northBtnDisable();
-				   southBtnDisable();
-				   eastBtnDisable();
-				   westBtnDisable();
-                 }
-                    
-				
-				
+			}
 		
-			/*function updateLocation(){
-				switch (currentLocation) {
-				case "default":
-					updateText("starting location" + direction)
-					break;
-				case "north1":
-					updateText("north1" + direction)
-					break;
-				case "south1":
-					updateText("south1" + direction)
-					break;
-				case "east1":
-					updateText("east1" + direction)
-					break;
-				case "west1":
-					updateText("west1" + direction)
-					break;
-				case "north2":
-					updateText("north2" + direction)
-					break;
-				default:
-					updateText("Unknown direction" + direction)
-				}
-			}*/
-			
-			//location text functions
-	  function defaultloc(){
-		defaultlocation = new Location (currentLocation, "defaultloc", "the default location", "ItemX");
-        updateText(defaultlocation)
-		location_itemx = new item ("01", "ItemX", "insert item description")
-		updateText (location_itemx)
-      }
-      function north2(){
-		northtwo = new Location (currentLocation, "north2", "x=0, y=2", "No Item");
-        updateText(northtwo)
-      }
-      function north1(){
-		northone = new Location (currentLocation, "north1", "x=0, y=1", "No Item");
-        updateText(northone)
-      }
-      function east2(){
-		easttwo = new Location (currentLocation, "east2", "x=2, y=0", "No Item");
-        updateText(easttwo)
-      }
-      function east1(){
-		eastone = new Location (currentLocation, "east1", "x=1, y=0", "Item Y");
-        updateText(eastone)
-		location_itemy = new item ("02", "ItemY", "insert item description")
-		updateText (location_itemy)
-      }
-      function west2(){
-		westtwo = new Location (currentLocation, "west", "x=-2, y=0", "No Item");
-        updateText(westtwo)
-      }
-      function west1(){
-		westone = new Location (currentLocation, "west1", "x=-1, y=0", "Item Z");
-        updateText(westone)
-		location_itemz = new item ("03", "ItemZ", "insert item description")
-		updateText (location_itemz)
-      }
-      function south2(){
-		southtwo = new Location (currentLocation, "south2", "x=0, y=-2", "No Item");
-        updateText(southtwo)
-      }
-      function south1(){
-		southone = new Location (currentLocation, "south1", "x=0, y=-1", "No Item");
-        updateText(southone)
-      }
-	  function northwest1(){
-		northwestone = new Location (currentLocation, "northwest1", "x=-1, y=1", "Item A");
-		updateText(northwestone)
-		location_itema = new item ("04", "ItemA", "insert item description")
-		updateText (location_itema)
-	  }
-        
+		
 			//to have dynamic text updates
 			function updateText(msg){
 				msg = msg;
@@ -292,27 +114,31 @@
 			//Can't figure out how to not allow infinite item pickups with an array.
 			function take(){
 				var check = document.getElementById("txtcmd");
-				if ((currentLocation = "default") && (check.value === "take item x")){
-					updateText("Taken Item X")
-					inventory.push("ItemX")
+				if ((currentLocation = "north2") && (check.value === "take fragment 1")){
+					updateText("Taken fragment 1.")
+					inventory.push("Key fragment 1")
+					frag1Check = 1
 				}
-				if ((currentlocation = "west1") && (check.value === "take item z")){
-					updateText ("Taken Item Z")
-					inventory.push("ItemZ")
+				if ((currentlocation = "west1") && (check.value === "take fragment 3")){
+					updateText ("Taken Fragment 2.")
+					inventory.push("Key fragment 3")
+					frag3Check = 1
 				}
-				if ((currentlocation = "northwest1") && (check.value === "take item a")){
-					updateText ("Taken Item A")
-					inventory.push("ItemA")
+				if ((currentlocation = "south2") && (check.value === "take key frame")){
+					updateText ("Taken key frame.")
+					inventory.push("Key frame")
+					frameCheck = 1
 				}
-				if ((currentlocation = "east1") && (check.value === "take item y")){
-					updateText ("Taken Item Y")
-					inventory.push("ItemY")
+				if ((currentlocation = "east1") && (check.value === "take fragment 2")){
+					updateText ("Taken fragment 2.")
+					inventory.push("Key fragment 2")
+					frag2Check = 1
 				}
-				else updateText("");
+				else updateText("You can't pick up dust.");
 			}
 
 				
-			//visually disable buttons based on currentLocation
+			//visually disable buttons depending on currentLocation
 			
 				function northBtnDisable(){
 				  if ((currentLocation == "north2") || (currentLocation == "east2") || 
@@ -382,8 +208,4 @@
 						return retVal;
 						}
 					}
-				
-				
-				
-				
 	
